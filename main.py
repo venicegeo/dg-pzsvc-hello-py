2a2df8607c99473f1a2a4f22d972afd8c22ef42b
Grant Soyka <gsoyka@gmail.com>
2017-05-25 08:40:15 -0600

    Added port definition to work with pcf network

--- main.py
+++ main.py
@@ -21,6 +21,6 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-
+port = os.getenv('VCAP_APP_PORT', '5000')
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=int(port))
